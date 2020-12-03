d174e377a0eeabef4985b1eed6cc53c9ebecefbf
zhangjh_oss <2874204690@qq.com>
2020-12-03 13:57:42 +0800

    添加动态menu

--- static/js/extends/system.js
+++ static/js/extends/system.js
@@ -1,3 +1,9 @@
+/**
+ * 系统工具，采用window方式定义不需要显示调用
+ * 定义全局变量，基本每个页面都需要引入的模块
+ * 目前没有自动引入，需要手动引入
+ * 封装了ajax工具
+ **/
 layui.define(['jquery', 'form', 'layer', 'element'], function(exports) {
     window.basePath = "http://localhost:8080/";
     window.client_id = "client";
@@ -21,14 +27,27 @@ layui.define(['jquery', 'form', 'layer', 'element'], function(exports) {
                 //......
                 beforeSend:function (XHR) {
                     //附带token信息，设置header值
-                    if(localStorage.getItem("auto_token")!=""&&localStorage.getItem("auto_token")!=null){
-                        XHR.setRequestHeader("Authorization",localStorage.getItem("auto_token"));
+                    if(localStorage.getItem("auto_token")!=null&&localStorage.getItem("auto_token")!="undefined"&&localStorage.getItem("auto_token")!=""){
+                        XHR.setRequestHeader("Authorization","Bearer "+localStorage.getItem("auto_token"));
+                        //XHR.setRequestHeader("Content-Type","");
+                        if(params.url.indexOf("/oauth/token")==-1){//非token请求都采用json格式
+                            XHR.setRequestHeader("Content-Type","application/json");
+                        }
                     }else{
                         XHR.setRequestHeader("Authorization","");
                     }
                     this.layerIndex = layer.load(1);
                 },
                 success:function(data){
+                    if(data.code=="A0230"){
+                        layer.close(this.layerIndex);
+                        layer.msg('登录已过期，请重新登录！',{icon: 5});//失败
+                        localStorage.setItem("auto_token","");
+                        setTimeout(function(){
+                            window.location.href='./login.html';
+                        },2000);
+                        return
+                    }
                     layer.close(this.layerIndex);
                     resolve(data)
                 },
@@ -37,12 +56,7 @@ layui.define(['jquery', 'form', 'layer', 'element'], function(exports) {
                     reject(error)
                 },
                 complete: function (XHR, TS) {
-                    layer.close(this.layerIndex);
-                    //验证是否token过期请求，如果过期则重定向至登录页面
-                    if(TS=="error"){
-                        layer.msg('登录已过期，请重新登录！',{icon: 5});//失败
-                        window.location.href='./login.html';
-                    }
+
                 },
             });
         })
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,66 +1,45 @@
-/*
- * @Author: https://github.com/WangEn
- * @Author: https://gitee.com/lovetime/
- * @Date:   2019-06-18
- * @lastModify 2019-06-20 11:45:41
- * +----------------------------------------------------------------------
- * | Weadmin [ 后台管理模板 ]
- * | 基于Layui http://www.layui.com/
- * +----------------------------------------------------------------------
- */
 /**
- * 扩展一个Menu模块，动态获取左侧菜单
- * 目前多级子菜单是通过延时循环获取的方式
- * 也可以采用点击时获取下级子菜单的方式
- * 当然，具体的情况要参照自己的需求及api返回结构
+ * 扩展一个Menu模块，获取左侧菜单
+ * 目前菜单栏是通过登录时获取的用户菜单权限进行构造
  **/
-
 layui.define('jquery', function(exports) {
 	var $ = layui.jquery;
 	var menu = {
-		getMenu: function(curl) {
-			var that = this;
-			if (!$('#side-nav').length) {
-				return;
-			}
-			$.getJSON(curl, function(res) {
-				var menuList = res.data;
-				var items = [];
-				var _ul = $('<ul></ul>').addClass('nav').attr('id', 'nav');
-				$.each(menuList, function(index, val) {
-					var item = '<li id="menu' + val.id + '"><a _href="' + val.url + '"><i class="iconfont">' + val.icon +
-						'</i><cite>' + val.name +
-						'</cite><i class="iconfont nav_right">&#xe697;</i></a></li>';
-
+		getMenu: function() {
+			console.log(JSON.parse(localStorage.getItem("menuList")));
+			console.log(JSON.parse(localStorage.getItem("userInfo")));
+			var menuList = JSON.parse(localStorage.getItem("menuList"));
+			var items = [];
+			var _ul = $('<ul></ul>').addClass('nav').attr('id', 'nav');
+			$.each(menuList, function(index, val) {
+				if(val.parentid==-1){
+					var item = '<li id="menu' + val.id + '"><a _href="' + val.pageUrl + '"><i class="layui-icon '+val.icon+'">' +
+					'</i><cite>' + val.name +
+					'</cite><i class="iconfont nav_right">&#xe697;</i></a></li>';
 					items.push(item);
-					if (val.children && val.children.length) {
-						//有二级子菜单
-						setTimeout(function() {
-							that.getSubMenu(val.id, val.children);
-						}, 500);
-					}
-				});
-				$('#side-nav').empty();
-				_ul.append(items.join(''));
-				$('#side-nav').append(_ul);
+					//拼接子菜单
+					setTimeout(function() {
+						menu.getSubMenu(val.id);
+					}, 500);
+				}
 			});
+			$('#side-nav').empty();
+			_ul.append(items.join(''));
+			$('#side-nav').append(_ul);
 		},
 		/**
-		 * @param {Object} id 上级子菜单id,拼接menu获取元素
-		 * @param {Object} subList 子菜单列表
+		 * @param {Number} id 上级子菜单id,拼接menu获取元素
+		 * 目前仅支持两级菜单，需要三级菜单需要进行改造
 		 */
-		getSubMenu: function(id, subList) {
-			var that = this;
+		getSubMenu: function(id) {
+			var menuList = JSON.parse(localStorage.getItem("menuList"));
 			var subItems = [];
 			var subUl = $('<ul></ul>').addClass('sub-menu');
-			$.each(subList, function(idx, sub) {
-				var subItem = '<li id="menu' + sub.id + '"><a _href="' + sub.url + '"><i class="iconfont">&#xe6a7;</i><cite>' +
-					sub.name + '</cite></a></li>';
-				subItems.push(subItem);
-				if (sub.children && sub.children.length) {
-					setTimeout(function() {
-						that.getSubMenu(sub.id, sub.children);
-					}, 1500);
+			$.each(menuList, function(index,menu) {
+				if(menu.leafis=="1"){//叶子节点
+					var subItem = '<li id="menu' + menu.id + '"><a _href="' + menu.url + '"><i class="iconfont">&#xe6a7;</i><cite>' +
+					menu.name + '</cite></a></li>';
+					subItems.push(subItem);
 				}
 			});
 			subUl.append(subItems.join(''));
